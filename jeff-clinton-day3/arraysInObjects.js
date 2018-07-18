b2a27dda7a321bd703ccbd7e37d8cd1fc11d8797
maxjeffwell <maxjeffwell@gmail.com>
2018-07-18 14:56:00 -0400

    added arraysInObjects.js

---
+++ jeff-clinton-day3/arraysInObjects.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const object = {
+  meals: [
+    'breakfast',
+    'second breakfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
+  ]
+}; 
+
+console.log(object.meals[3]);
